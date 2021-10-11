4ddcf5fe42cb72f0515fa58656a9f0a6ac4e5561
Cornelius <bcornelius@iseatz.com>
2021-10-11 14:31:13 -0500

    updates contact styling

--- src/Mocks/MockContactWidget/MockContactWidget.jsx
+++ src/Mocks/MockContactWidget/MockContactWidget.jsx
@@ -5,7 +5,6 @@ import './MockContactWidget.css';
 
 export const MockContactWidget = () => <Wedge>
     <div className="contact-container">
-        <h3>Contact Us!<br/><small>Send us an email and we will get back to you as soon as possible.</small></h3>
         <Contact />
     </div>
 </Wedge>
