1ce1c0caf02d6bc635c590f8838b9f0eb5d78a33
Cornelius <bcornelius@iseatz.com>
2021-10-10 23:32:18 -0500

    adds contact widfet

--- src/Mocks/MockContactWidget/MockContactWidget.jsx
+++ src/Mocks/MockContactWidget/MockContactWidget.jsx
@@ -4,5 +4,8 @@ import { Contact } from '../../components/Contact/Contact';
 import './MockContactWidget.css';
 
 export const MockContactWidget = () => <Wedge>
-    <Contact />
+    <div className="contact-container">
+        <h3>Contact Us!<br/><small>Send us an email and we will get back to you as soon as possible.</small></h3>
+        <Contact />
+    </div>
 </Wedge>
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Contact.css';
 
-export const Contact = () => <>
-    <div className="column-one">
-        <div className="two-input-container">
-            <label for="first-name">First Name</label>
-            <input type="text" id="first-name" name="first-name" />
-            <label for="last-name">Last Name</label>
-            <input type="text" id="last-name" name="last-name" />
+const InputContainer = ({children}) => <div className="input-container">{children}</div>
+
+export const Contact = () => {
+    const [selection, setSelection] = useState("")
+    const checkSelection = targetValue => targetValue === selection;
+
+    return <div className="container-row">
+        <div className="column">
+            <div className="two-input-container">
+                <InputContainer>
+                    <label for="first-name">First Name</label>
+                    <input type="text" id="first-name" name="first-name" />
+                </InputContainer>
+                <InputContainer>
+                    <label for="last-name">Last Name</label>
+                    <input type="text" id="last-name" name="last-name" />
+                </InputContainer>
+            </div>
+            <div className="two-input-container">
+                <InputContainer>
+                    <label for="email">Email</label>
+                    <input type="text" id="email" name="email" />
+                </InputContainer>
+                <InputContainer>
+                    <label for="phone">Phone Number</label>
+                    <input type="text" id="phone" name="phone" />
+                </InputContainer>
+            </div>
+            <div className="radio-input-container">
+                <label className="button-label">Are you a new client?</label>
+                <div className="button-container">
+                    <button className={checkSelection("yes") ? "active" : "inactive"} onClick={() => setSelection("yes")}>Yes</button>
+                    <button className={checkSelection("no") ? "active" : "inactive"} onClick={() => setSelection("no")}>No</button>
+                </div>
+            </div>
         </div>
-        <div clasNames="two-input-container">
-            <label for="email">Email</label>
-            <input type="text" id="email" name="email" />
-            <label for="phone">Phone Number</label>
-            <input type="text" id="phone" name="phone" />
-        </div>
-        <div className="radio-input-container">
-            <label for="new-client">Are you a new customer?</label>
-            <input type="radio" is="new-client" name="new-client" />
+        <div className="column">
+            <div className="input-message-container">
+                <label for="message">Message:</label>
+                <input className="contact-message" type="text" id="message" />
+            </div>
         </div>
     </div>
-</>
+}
