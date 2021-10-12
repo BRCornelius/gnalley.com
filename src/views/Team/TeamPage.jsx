b91c152ece6daada53fd4212a362779534cdf845
Cornelius <bcornelius@iseatz.com>
2021-10-12 14:27:43 -0500

    pre first deployment

--- src/views/Team/TeamPage.jsx
+++ src/views/Team/TeamPage.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import './TeamPage.css';
 
 const teamMembers = [{
@@ -37,32 +38,40 @@ const teamMembers = [{
 export const TeamPage = () => {
     const [teamMember, setTeamMember] = useState(null)
 
-    const TeamMemberDisplay = ({teamMember, selected}) => <div className="team-member-display" onClick={() => setTeamMember(teamMember)}>
-        <img alt="member" className="team-member-image" src={teamMember.img}/>
-        <h3 className="team-member-description">{teamMember.name}<br/><small>{teamMember.title}</small></h3>
+    const history = useHistory();
+    const handleClick = () => {
+        history.push("/contact");
+    }
+
+    const TeamMemberDisplay = ({member, selected}) => <div className="team-member-display" onClick={() => setTeamMember(member)}>
+        <img alt="member" className="team-member-image" src={member.img}/>
+        <h3 className="team-member-description">{member.name}<br/><small>{member.title}</small></h3>
         {selected && teamMember.description}
     </div>
 
     return <div className="single-page-container">
         {!teamMember && <div className="team-page-container">
             <div className="team-row">
-                <TeamMemberDisplay teamMember={teamMembers[0]} />
-                <TeamMemberDisplay teamMember={teamMembers[1]} />
+                <TeamMemberDisplay member={teamMembers[0]} />
+                <TeamMemberDisplay member={teamMembers[1]} />
             </div>
             <div className="team-row">
-                <TeamMemberDisplay teamMember={teamMembers[2]} />
-                <TeamMemberDisplay teamMember={teamMembers[3]} />
+                <TeamMemberDisplay member={teamMembers[2]} />
+                <TeamMemberDisplay member={teamMembers[3]} />
             </div>
             <div className="team-row">
-                <TeamMemberDisplay teamMember={teamMembers[4]} />
+                <TeamMemberDisplay member={teamMembers[4]} />
             </div>
         </div>}
         {teamMember && <div className="team-page-container-selected">
             <div className="team-left-column">
-                {teamMembers.map(member => member.name === teamMember.name ? <></> : <TeamMemberDisplay teamMember={member} />)}
+                {teamMembers.map(member => member.name === teamMember.name ? <></> : <TeamMemberDisplay member={member} />)}
             </div>
             <div className="team-right-column">
-                <TeamMemberDisplay teamMember={teamMember} selected/>
+                <TeamMemberDisplay member={teamMember} selected/>
+                <div className="team-button-container">
+                    <button className="team-contact-button" onClick={handleClick}>Contact Me</button>
+                </div>
             </div>
         </div>}
     </div>
