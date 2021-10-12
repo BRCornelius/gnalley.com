85593a2123c37fdfdd5181b11cf9c6841f4517a5
Cornelius <bcornelius@iseatz.com>
2021-10-12 10:08:42 -0500

    adjusts styles for svp

--- src/widgets/FirmWidget/FirmWidget.jsx
+++ src/widgets/FirmWidget/FirmWidget.jsx
@@ -4,9 +4,14 @@ import './FirmWidget.css';
 
 export const FirmWidget = () => <Wedge>
     <div className="outer-container">
+        <h1>Who we are</h1>
         <div className="general-container">
-            <h1>Who we are</h1>
-            <p className="general-widget">Founded in 1990 by George Nalley, Nalley and Dew, APLC is a Louisiana law firm with a global perspective. The firm’s practice is litigation centered, with extensive experience in all areas of casualty litigation, including maritime, products liability, and environmental law issues, as well as state and federal workers’ compensation. The firm also has a strong foundation in employment and labor law litigation and insurance coverage issues. We have been privileged to represent our clients in a variety of civil and maritime business disputes, including construction and contractor litigation, and civil RICO. While our practice is predominately in Louisiana, our attorneys are licensed in Louisiana, Mississippi, Texas, and Florida, and Nalley and Dew has tried and/or handled cases in numerous other states, including  Florida, Tennessee, and Ohio. We proudly represent clients in both defense and plaintiff litigation matters.</p>
+            <p className="general-widget">Founded in 1990 by George Nalley, <strong>Nalley and Dew, APLC</strong> is a Louisiana law firm with a global perspective.</p>
+            <p>We are litigation centered, with extensive experience in all areas of casualty litigation, including maritime, products liability, and environmental law issues, as well as state and federal workers’ compensation.</p>
+            <p>We have a strong foundation in employment and labor law litigation and insurance coverage issues.</p>
+            <p>We have been privileged to represent our clients in a variety of civil and maritime business disputes, including construction and contractor litigation, and civil RICO.</p>
+            <p>While our practice is predominately in Louisiana, our attorneys are licensed in Louisiana, Mississippi, Texas, and Florida, and Nalley and Dew has tried and/or handled cases in numerous other states, including Tennessee and Ohio.</p>
+            <p>We proudly represent clients in both defense and plaintiff litigation matters.</p>
         </div>
     </div>
     </Wedge>
