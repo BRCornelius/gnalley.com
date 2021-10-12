36f733d42cab50536ebb67694bc6edfbe26439b7
Cornelius <bcornelius@iseatz.com>
2021-10-12 00:30:04 -0500

    updates firm copy

--- src/Mocks/MockGeneralWidget/MockGeneralWidget.jsx
+++ src/Mocks/MockGeneralWidget/MockGeneralWidget.jsx
@@ -3,8 +3,10 @@ import { Wedge } from '../../components';
 import './MockGeneralWidget.css';
 
 export const MockGeneralWidget = () => <Wedge>
+    <div className="outer-container">
         <div className="general-container">
-            <h1>Widget Label</h1>
-            <p className="general-widget">The first Wedge</p>
+            <h1>Who we are</h1>
+            <p className="general-widget">Founded in 1990 by George Nalley, Nalley and Dew, APLC is a Louisiana law firm with a global perspective. The firm’s practice is litigation centered, with extensive experience in all areas of casualty litigation, including maritime, products liability, and environmental law issues, as well as state and federal workers’ compensation. The firm also has a strong foundation in employment and labor law litigation and insurance coverage issues. We have been privileged to represent our clients in a variety of civil and maritime business disputes, including construction and contractor litigation, and civil RICO. While our practice is predominately in Louisiana, our attorneys are licensed in Louisiana, Mississippi, Texas, and Florida, and Nalley and Dew has tried and/or handled cases in numerous other states, including  Florida, Tennessee, and Ohio. We proudly represent clients in both defense and plaintiff litigation matters.</p>
         </div>
+    </div>
     </Wedge>
--- src/views/Contact/ContactPage.jsx
+++ src/views/Contact/ContactPage.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Contact } from '../../components';
 import './ContactPage.css';
 
-export const ContactPage = () => <div className="single-page-container">
+export const ContactPage = () => <div className="contact-page-container">
     <Contact isWidget={false}/>
     <div className="two-columns">
         <div className="vertical-column">
             <h1>Call us:</h1>
+            <p>Phone: (504) 838-8188<br/>Fax: (504) 838-0008</p>
         </div>
-        <div className="vertical-column">
+        <div className="vertical-column-right">
             <h1>Our office:</h1>
             <p>2450 Severn Avenue<br/>Suite 100<br/>Metairie, LA 70001</p>
         </div>
