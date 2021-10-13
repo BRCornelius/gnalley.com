7ec8bedb4c9a451386e5025f6e3c52ebe4b83b54
Cornelius <bcornelius@iseatz.com>
2021-10-12 23:36:51 -0500

    updates mobile of team page

--- src/views/Team/TeamPage.jsx
+++ src/views/Team/TeamPage.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import { teamMembers } from '../../utils';
+import { isMobile, teamMembers } from '../../utils';
 import './TeamPage.css';
 
 export const TeamPage = () => {
     const [teamMember, setTeamMember] = useState(null)
+    const checkTeamMember = targetMember => teamMember ? teamMember.name === targetMember.name : false;
 
     const history = useHistory();
     const handleClick = () => {
@@ -17,30 +18,40 @@ export const TeamPage = () => {
         {selected && teamMember.description}
     </div>
 
+    const MobileTeamMemberDisplay = ({member}) => <div onClick={() => setTeamMember(member)}>
+        <img alt="member" className="team-member-image" src={member.img}/>
+        <h3 className="team-member-description">{member.name}<br/><small>{member.title}</small></h3>
+        {checkTeamMember(member) && teamMember.description}
+    </div>
+
     return <div className="single-page-container">
-        {!teamMember && <div className="team-page-container">
-            <div className="team-row">
-                <TeamMemberDisplay member={teamMembers[0]} />
-                <TeamMemberDisplay member={teamMembers[1]} />
-            </div>
-            <div className="team-row">
-                <TeamMemberDisplay member={teamMembers[2]} />
-                <TeamMemberDisplay member={teamMembers[3]} />
-            </div>
-            <div className="team-row">
-                <TeamMemberDisplay member={teamMembers[4]} />
-            </div>
-        </div>}
-        {teamMember && <div className="team-page-container-selected">
-            <div className="team-left-column">
-                {teamMembers.map(member => member.name === teamMember.name ? <></> : <TeamMemberDisplay member={member} />)}
-            </div>
-            <div className="team-right-column">
-                <TeamMemberDisplay member={teamMember} selected/>
-                <div className="team-button-container">
-                    <button className="team-contact-button" onClick={handleClick}>Contact Me</button>
+        {isMobile ? <>
+            {teamMembers.map(member => <MobileTeamMemberDisplay member={member} />)}
+        </> : <>
+            {!teamMember && <div className="team-page-container">
+                <div className="team-row">
+                    <TeamMemberDisplay member={teamMembers[0]} />
+                    <TeamMemberDisplay member={teamMembers[1]} />
                 </div>
-            </div>
-        </div>}
+                <div className="team-row">
+                    <TeamMemberDisplay member={teamMembers[2]} />
+                    <TeamMemberDisplay member={teamMembers[3]} />
+                </div>
+                <div className="team-row">
+                    <TeamMemberDisplay member={teamMembers[4]} />
+                </div>
+            </div>}
+            {teamMember && <div className="team-page-container-selected">
+                <div className="team-left-column">
+                    {teamMembers.map(member => member.name === teamMember.name ? <></> : <TeamMemberDisplay member={member} />)}
+                </div>
+                <div className="team-right-column">
+                    <TeamMemberDisplay member={teamMember} selected/>
+                    <div className="team-button-container">
+                        <button className="team-contact-button" onClick={handleClick}>Contact Me</button>
+                    </div>
+                </div>
+            </div>}
+        </>}
     </div>
 };
