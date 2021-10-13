0058063878d8fbaa9d25e436bbd3486db15a73cf
Cornelius <bcornelius@iseatz.com>
2021-10-12 22:54:16 -0500

    adds mobile styling for team widget

--- src/components/Wedge/Wedge.jsx
+++ src/components/Wedge/Wedge.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { isMobile } from '../../utils/browserUtils';
 import './Wedge.css';
 
-export const Wedge = ({children}) => <div className="Wedge-container">{children}</div>
+export const Wedge = ({children}) => <div className={isMobile ? "mobile-wedge-container" : "Wedge-container"}>{children}</div>
--- src/widgets/TeamWidget/TeamWidget.jsx
+++ src/widgets/TeamWidget/TeamWidget.jsx
@@ -2,6 +2,7 @@ import React, { useRef } from 'react';
 import { useHistory } from 'react-router-dom';
 import { Rerousel } from 'rerousel';
 import {Wedge} from '../../components';
+import { isMobile } from '../../utils/browserUtils';
 import './TeamWidget.css';
 
 export const TeamWidget = () => {
@@ -15,12 +16,12 @@ export const TeamWidget = () => {
             <h1>Meet our Team:</h1>
         </div>
         <div className="carousel-container">
-            <Rerousel itemRef={initialRef} stop={false} interval={2000}>
-                <img alt="first" className="photo" src="http://www.gnalley.com/wp-content/uploads/2014/09/GeorgeDSC_4698.jpg" ref={initialRef} />
-                <img alt="second" className="photo" src="http://www.gnalley.com/wp-content/uploads/2014/09/DSC_2585.jpg" />
-                <img alt="third" className="photo" src="http://www.gnalley.com/wp-content/uploads/2014/09/DSC_2415.jpg" />
-                <img alt="fourth" className="photo" src="http://www.gnalley.com/wp-content/uploads/2014/09/DSC_2350.jpg" />
-                <img alt="fourth" className="photo" src="http://www.gnalley.com/wp-content/uploads/2014/08/DSC_2488.jpg" />
+            <Rerousel itemRef={initialRef} stop={isMobile ? true : false} interval={2000}>
+                <img alt="first" className={isMobile ? "mobile-photo" : "photo"} src="http://www.gnalley.com/wp-content/uploads/2014/09/GeorgeDSC_4698.jpg" ref={initialRef} />
+                <img alt="second" className={isMobile ? "mobile-photo" : "photo"} src="http://www.gnalley.com/wp-content/uploads/2014/09/DSC_2585.jpg" />
+                <img alt="third" className={isMobile ? "mobile-photo" : "photo"} src="http://www.gnalley.com/wp-content/uploads/2014/09/DSC_2415.jpg" />
+                <img alt="fourth" className={isMobile ? "mobile-photo" : "photo"} src="http://www.gnalley.com/wp-content/uploads/2014/09/DSC_2350.jpg" />
+                <img alt="fourth" className={isMobile ? "mobile-photo" : "photo"} src="http://www.gnalley.com/wp-content/uploads/2014/08/DSC_2488.jpg" />
             </Rerousel>
         </div>
     </Wedge>
