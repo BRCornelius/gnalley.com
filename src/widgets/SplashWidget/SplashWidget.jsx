7fb8d1edf4766b37ea3cb51263c19c67bf91bf18
Cornelius <bcornelius@iseatz.com>
2021-10-15 22:15:15 -0500

    resizes splash image

--- src/widgets/SplashWidget/SplashWidget.jsx
+++ src/widgets/SplashWidget/SplashWidget.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Wedge } from '../../components';
+import { isMobile } from '../../utils';
 import './SplashWidget.css'
 
 export const SplashWidget = () => <Wedge>
     <div className="splash-container">
-        <img alt="office" className="splash-image" src="http://www.gnalley.com/wp-content/uploads/2018/12/Severn-Exterior-photo-North-Side.jpg" />
+        <img alt="office" className={isMobile ? "full-image" : "splash-image"} src="http://www.gnalley.com/wp-content/uploads/2018/12/Severn-Exterior-photo-North-Side.jpg" />
     </div>
 </Wedge>
