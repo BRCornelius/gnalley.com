5573b60e7ac175426b2ed3095df7f448216bd7af
Cornelius <bcornelius@iseatz.com>
2021-10-18 23:20:22 -0500

    uses new email service

--- src/utils/apiUtils.js
+++ src/utils/apiUtils.js
@@ -1,10 +1,3 @@
-const formatContactDataIntoMessage = ({firstName, lastName, email, phone, newCustomer, message}) => ({
-    body: newCustomer
-        ? `You have received a message from a new client, ${firstName} ${lastName}.  It reads: ${message}.  You can contact ${firstName} ${lastName} at ${phone}.`
-        : `You have received a message from an existing client, ${firstName} ${lastName}.  It reads: ${message}.  You can contact ${firstName} ${lastName} at ${phone}.`,
-    from: email
-})
-
 export const submitValues = values => {
     const xhr = new XMLHttpRequest();
     xhr.addEventListener('submitting values', () => {
@@ -12,5 +5,5 @@ export const submitValues = values => {
     })
     xhr.open('POST', 'https://services.corneliuses.com/sendEmail')
     xhr.setRequestHeader('X-Api-Key', process.env.REACT_APP_APIKEY)
-    xhr.send(JSON.stringify(formatContactDataIntoMessage(values)))
+    xhr.send(JSON.stringify(values))
 }
